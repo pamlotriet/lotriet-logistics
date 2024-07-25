06024842923c13f4853d903a40c7195999baa5d9
Pamela Lotriet <plotriet@retrorabbit.co.za>
2024-07-25 12:17:12 +0200

    updated lazy loading componenta

--- lotriet-logistics/src/app/app.routes.ts
+++ lotriet-logistics/src/app/app.routes.ts
@@ -3,24 +3,24 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    loadChildren: () =>
+    loadComponent: () =>
       import('./pages/home-page/home-page.component').then((m) => m.HomePageComponent),
   },
   {
     path: 'about',
-    loadChildren: () =>
+    loadComponent: () =>
       import('./pages/about-page/about-page.component').then((m) => m.AboutPageComponent),
   },
   {
     path: 'services',
-    loadChildren: () =>
+    loadComponent: () =>
       import('./pages/services-pages/services-pages.component').then(
         (m) => m.ServicesPagesComponent,
       ),
   },
   {
     path: 'contact',
-    loadChildren: () =>
+    loadComponent: () =>
       import('./pages/contact-page/contact-page.component').then((m) => m.ContactPageComponent),
   },
 ];
